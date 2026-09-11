71181047c29ea057bbc9c343befbfa6e3b450da7
ChitSuHlaing143 <chitsuhlaing.cshta@gmail.com>
2026-09-11 10:31:26 +0630

    Update DELIVERY_DEMO_CONFIG for Google Sheets usage

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.DELIVERY_DEMO_CONFIG = {
   COMPANY_NAME: 'SwiftDrop Delivery Demo',
-  GOOGLE_APPS_SCRIPT_URL: '',
-  USE_GOOGLE_SHEETS: false
+  GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyETRb9LGSfwpqB38MJbZt8P8HUKH25PuITun4KogxXtJVfPIhrmzbxbEXJ0wr-7J7A-g/exec',
+  USE_GOOGLE_SHEETS: true
 };
